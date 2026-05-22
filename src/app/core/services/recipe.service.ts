5d87493871b84812f4ab3fc57c541066322a2a34
Julie <julie.cervera@steerfox.com>
2026-05-22 11:41:01 +0200

    FEAT: add recipes list service

---
+++ src/app/core/services/recipe.service.ts
@@ -0,0 +1,20 @@
+import { inject, Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+// TODO: add custom paths for imports
+import { Meal, MealResponse } from '../models/meal.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  httpClient = inject(HttpClient);
+
+  constructor() {}
+
+  getRecipes(): Observable<Meal[]> {
+    return this.httpClient
+      .get<MealResponse>('https://www.themealdb.com/api/json/v1/1/search.php?f=b')
+      .pipe(map((res) => res.meals));
+  }
+}
