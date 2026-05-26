412e70f0119e6d68eaf7059eea93678906e64fe0
Julie <julie.cervera@steerfox.com>
2026-05-26 22:15:08 +0200

    FEAT: add favorite/todo buttons

--- src/app/detail/detail.ts
+++ src/app/detail/detail.ts
@@ -3,20 +3,25 @@ import { RecipeService } from '../core/services/recipe.service';
 import { Observable } from 'rxjs';
 import { Meal } from '../core/models/meal.model';
 import { Router } from '@angular/router';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, Location, NgClass } from '@angular/common';
+import { StorageKeys, StorageService } from '../core/services/storage.service';
 
 @Component({
   selector: 'app-detail',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, NgClass],
   templateUrl: './detail.html',
   styleUrl: './detail.css',
 })
 export class Detail implements OnInit {
   recipeService = inject(RecipeService);
+  storageService = inject(StorageService);
+  location = inject(Location);
   router = inject(Router);
   recipe$: Observable<Meal> | undefined;
   recipeId: string | undefined;
   error = signal<string | null>(null);
+  isFavorite = signal(false);
+  isToDo = signal(false);
 
   constructor() {
     // TODO better way to extract id from url
@@ -31,6 +36,28 @@ export class Detail implements OnInit {
     }
     if (!this.recipe$) {
       this.error.set('Recipe not found');
+    }
+  }
+
+  back(): void {
+    this.location.back();
+  }
+
+  addToFavorites() {
+    this.isFavorite.set(!this.isFavorite());
+    if (this.isFavorite()) {
+      this.storageService.addToLocalStorage(StorageKeys.FAVORITES, this.recipeId);
+    } else {
+      this.storageService.removeItemFromLocalStorage(StorageKeys.FAVORITES, this.recipeId!);
+    }
+  }
+
+  addToDo() {
+    this.isToDo.set(!this.isToDo());
+    if (this.isToDo()) {
+      this.storageService.addToLocalStorage(StorageKeys.TODO, this.recipeId);
+    } else {
+      this.storageService.removeItemFromLocalStorage(StorageKeys.TODO, this.recipeId!);
     }
   }
 }
